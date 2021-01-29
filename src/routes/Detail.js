ddb711aeb1d6d484b09b42b7174e8bc0522ee65d
Sang-heon <sangheon@dayoff.ai>
2021-01-29 21:49:54 +0900

    detail data add

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,13 +3,35 @@ import React from "react";
 
 class Detail extends React.Component{
     componentDidMount() {
-        const { location } = this.props;
-        console.log(location.state.title)
+        const { location, history } = this.props;
+        if (location.state === undefined ) {
+            history.push("/")
+        }
         
     };
     render() {
         const { location } = this.props;
-        return <span>{location.state.title}</span>
+        console.log(location.state)
+        console.log(location.state.genres)
+
+        return (
+            <div className="movie__detail">
+                <div className="movie__detail__top">
+                    <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+                    <h2>{location.state.title}</h2>
+                    <em>{location.state.year}</em>
+                    <ul>
+                        {location.state.genres.map((item,index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="movie__detail__bot">
+                    <p>{location.state.summary}</p>
+                </div>
+                
+            </div>
+        )
     };
 }
 
